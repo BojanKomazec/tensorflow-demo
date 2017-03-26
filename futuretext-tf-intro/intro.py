bb7c032db8f879b9f4719211cb1f6bc53117b7cf
Bojan Komazec <bojan.komazec@gmail.com>
2017-03-26 13:30:40 +0100

    Added futuretext-tf-intro directory (contains TensorFlow scripts from Enterprise AI course)

---
+++ futuretext-tf-intro/intro.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+# Buiding computational graph
+node1 = tf.constant(3.0, tf.float32)
+node2 = tf.constant(4.0) # tf.float32 implicitly
+print(node1, node2)
+
+# Runnig computational graph
+sess = tf.Session()
+print(sess.run([node1, node2]))
