a9be2520335925686a60f2013974655710393f53
Bojan Komazec <bojan.komazec@gmail.com>
2019-10-21 00:15:54 +0100

    Added TensorFlow (v2) Keras-based MNIST image classifier demo.

--- src/tf-demo.py
+++ src/tf-demo.py
@@ -1,9 +1,11 @@
 from demo.install import testTensorFlowInstallation
 from demo.comp_graph import computationalGraphDemo
+from demo.keras import mnistDemo
 
 def main():
     testTensorFlowInstallation()
     computationalGraphDemo()
+    mnistDemo()
 
 if __name__ == "__main__":
     main()
