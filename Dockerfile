e807cde758ebe9dcb21ba302c5e69c76455428d3
Bojan Komazec <bojan.komazec@gmail.com>
2019-10-22 00:01:02 +0100

    Added support for running in virtual environment. Added code which prints and displays data in training/test sets.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM tensorflow/tensorflow:latest-gpu-py3 as tf-base
-RUN pip3 install --upgrade pip && pip3 install packaging
+WORKDIR /usr/local/tf-demo
+COPY requirements-docker.txt ./
+RUN pip3 install --upgrade pip && pip3 install -r requirements-docker.txt
 
 FROM tf-base
 WORKDIR /usr/local/tf-demo
--- src/demo/keras.py
+++ src/demo/keras.py
@@ -3,13 +3,42 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 # Install TensorFlow
 
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 def mnistDemo():
-	# Load and prepare the MNIST dataset. Convert the samples from integers to floating-point numbers:
-
+	# Load MNIST dataset
 	mnist = tf.keras.datasets.mnist
 
 	(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+	# Explore the data set.
+	# The MNIST database contains 60,000 training images and 10,000 testing images.
+	# Each image has size of 28x28 pixels and is grayscale-leveled.
+	print("x_train.shape:\n", x_train.shape)
+	print("x_train:\n", x_train)
+	print("x_train[0]:\n", x_train[0])
+
+	print("y_train.shape:\n", y_train.shape)
+	print("y_train:\n", y_train)
+	print("y_train[0]:\n", y_train[0])
+
+	print("x_test.shape:\n", x_test.shape)
+	print("x_test:\n", x_train)
+	print("y_test.shape:\n", y_test.shape)
+	print("y_test:\n", y_train)
+
+
+	first_array=x_train[0]
+	#Not sure you even have to do that if you just want to visualize it
+	#first_array=255*first_array
+	#first_array=first_array.astype("uint8")
+	plt.imshow(first_array)
+	#Actually displaying the plot if you are not in interactive mode
+	plt.show()
+	#Saving plot
+	plt.savefig("fig.png")
+
+	# prepare the MNIST dataset. Convert the samples from integers to floating-point numbers:
 	x_train, x_test = x_train / 255.0, x_test / 255.0
 
 	# Build the tf.keras.Sequential model by stacking layers. Choose an optimizer and loss function for training:
